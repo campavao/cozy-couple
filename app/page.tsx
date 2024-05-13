c5e8d8c0326b4546f902b2f3fcc8e75b02d87a8a
DESKTOP-8SRFDDM\cam95 <cam9548@gmail.com>
2024-05-13 10:42:39 -0500

    fix redirect loop

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,31 @@ import { isUserSubscribed } from "./api/apiUtils";
 
 async function Home() {
   const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return (
+      <div className='flex flex-col justify-center items-center h-[90%] gap-8'>
+        <h1 className='font-bold text-lg'>FlipTrack</h1>
+        <Login />
+      </div>
+    );
+  }
+
   const isSubscribed = await isUserSubscribed();
 
   return (
     <div className='flex flex-col justify-center items-center h-[90%] gap-8'>
       <h1 className='font-bold text-lg'>FlipTrack</h1>
-      {session ? (
-        <div className='flex flex-col gap-4 text-center'>
-          <Link href='/today'>Today</Link>
-          <Link href='/deliveries'>Deliveries</Link>
-          <Link href='/inventory'>Inventory</Link>
-          <Link href='/pickups'>Pickups</Link>
-          <Link href='/analytics'>Analytics</Link>
-          <Link href='/contact'>Contact</Link>
-          {!isSubscribed && <Link href='/subscription'>Subscribe</Link>}
-          <Login isSignout />
-        </div>
-      ) : (
-        <Login />
-      )}
+      <div className='flex flex-col gap-4 text-center'>
+        <Link href='/today'>Today</Link>
+        <Link href='/deliveries'>Deliveries</Link>
+        <Link href='/inventory'>Inventory</Link>
+        <Link href='/pickups'>Pickups</Link>
+        <Link href='/analytics'>Analytics</Link>
+        <Link href='/contact'>Contact</Link>
+        {!isSubscribed && <Link href='/subscription'>Subscribe</Link>}
+        <Login isSignout />
+      </div>
     </div>
   );
 }
