e7160dfe578cad92f6b4de01a4625fc04ae5b007
DESKTOP-8SRFDDM\cam95 <cam9548@gmail.com>
2024-05-26 11:59:58 -0500

    update with real payment link

--- app/constants.ts
+++ app/constants.ts
@@ -2,6 +2,6 @@ export const constants = {
   checkout:
     process.env.NODE_ENV === "development"
       ? "https://buy.stripe.com/test_8wM01L0DI5G126A6oo"
-      : "https://buy.stripe.com/test_7sI01LgCG3xT12waEF", // TODO update this for prod,
+      : "https://buy.stripe.com/eVabMo0pp2zZ24geUU",
   supportEmail: "fliptrackcouch@gmail.com",
 };
